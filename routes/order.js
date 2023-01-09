7b369b85c40910fb4d546427af4d122de2de9607
nthai3 <nthai3@cmcglobal.vn>
2023-01-09 15:59:27 +0700

    update order

--- routes/order.js
+++ routes/order.js
@@ -8,7 +8,7 @@ const router = Router();
 router.post("/orders", create);
 router.get("/orders/:id", read);
 router.get("/orders", list);
-router.put("/orders/:id/:userId", update);
+router.put("/orders/:id", update);
 router.delete("/orders/:id", remove);
 router.post("/order/create_payment_url", createPaymentUrl);
 router.get("/order/vnpay_return", vnpayReturn);
