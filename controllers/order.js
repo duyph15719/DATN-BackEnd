65c9ee55bcaec187ef383ba2688c846688920a3a
trung9901 <76927992+tung3122002@users.noreply.github.com>
2023-01-09 11:58:31 +0700

    VNPAY

--- controllers/order.js
+++ controllers/order.js
@@ -1,134 +1,273 @@
 import Order from "../models/order";
-
+import format from "date-format";
 export const create = async (req, res) => {
-    try {
-        const order = await new Order(req.body).save();
-        res.json(order);
-    } catch (error) {
-        res.status(400).json({
-            message: "Thêm đơn hàng thất bại",
-            error
-        });
-    }
+  try {
+    const order = await new Order(req.body).save();
+    res.json(order);
+  } catch (error) {
+    res.status(400).json({
+      message: "Thêm đơn hàng thất bại",
+      error,
+    });
+  }
 };
 
 export const read = async (req, res) => {
-    const filter = { _id: req.params.id };
-    const populate = req.query["_expand"];
+  const filter = { _id: req.params.id };
+  const populate = req.query["_expand"];
 
-    try {
-        const order = await Order.findOne(filter).select("-__v").populate(populate).exec();
-        res.json(order);
-    } catch (error) {
-        res.status(400).json({
-            message: "Không tìm thấy đơn hàng",
-            error
-        });
-    }
+  try {
+    const order = await Order.findOne(filter)
+      .select("-__v")
+      .populate(populate)
+      .exec();
+    res.json(order);
+  } catch (error) {
+    res.status(400).json({
+      message: "Không tìm thấy đơn hàng",
+      error,
+    });
+  }
 };
 
 export const list = async (req, res) => {
-    const populate = req.query["_expand"];
+  const populate = req.query["_expand"];
 
-    let sortOpt = {};
-    if (req.query["_sort"]) {
-        const sortArr = req.query["_sort"].split(",");
-        const orderArr = (req.query["_order"] || "").split(",");
-        
-        sortArr.forEach((sort, index) => {
-            sortOpt[sort] = orderArr[index] === "desc" ? -1 : 1;
-        });
-    }
+  let sortOpt = {};
+  if (req.query["_sort"]) {
+    const sortArr = req.query["_sort"].split(",");
+    const orderArr = (req.query["_order"] || "").split(",");
 
-    const start = req.query["_start"];
-    const limit = req.query["_limit"];
-
-    const filter = {};
-
-    const { _expand, _sort, _order, ...query } = req.query;
-    const queryArr = Object.keys(query);
-    queryArr.forEach(item => {
-        if (item.includes("like")) {
-            const objectKey = item.slice(0, item.indexOf("_"));
-
-            if (Object.hasOwn(filter, objectKey)) {
-                filter[objectKey]["$in"].push(new RegExp(req.query[item], "i"));
-            } else {
-                filter[objectKey] = {$in: [new RegExp(req.query[item], "i")]};
-            }
-        } else if (item.includes("_ne")) {
-            filter[item.slice(0, item.indexOf("_ne"))] = { $nin: query[item] };
-        } else if (item.includes("_gte")) {
-            const objectKey = item.slice(0, item.indexOf("_gte"));
-
-            if (Object.hasOwn(filter, objectKey)) {
-                filter[objectKey]["$gte"] = query[item];
-            } else {
-                filter[objectKey] = { $gte: query[item] };
-            }
-        } else if (item.includes("_lte")) {
-            const objectKey = item.slice(0, item.indexOf("_lte"));
-
-            if (Object.hasOwn(filter, objectKey)) {
-                filter[objectKey]["$lte"] = query[item];
-            } else {
-                filter[objectKey] = { $lte: query[item] };
-            }
-        } else if (item === "q" && query["q"]) {
-            filter["$text"] = {"$search": `"${query["q"]}"`};
-        } else {
-            if (Object.hasOwn(filter, item)) {
-                filter[item]["$in"].push(query[item]);
-            } else {
-                filter[item] = {$in: [query[item]]};
-            }
-        }
+    sortArr.forEach((sort, index) => {
+      sortOpt[sort] = orderArr[index] === "desc" ? -1 : 1;
     });
+  }
 
-    try {
-        const orders = await Order
-            .find(filter)
-            .select("-__v")
-            .populate(populate)
-            .skip(start)
-            .limit(limit)
-            .sort(sortOpt)
-            .exec();
-        res.json(orders);
-    } catch (error) {
-        res.status(400).json({
-            message: "Không tìm thấy đơn hàng",
-            error
-        });
+  const start = req.query["_start"];
+  const limit = req.query["_limit"];
+
+  const filter = {};
+
+  const { _expand, _sort, _order, ...query } = req.query;
+  const queryArr = Object.keys(query);
+  queryArr.forEach((item) => {
+    if (item.includes("like")) {
+      const objectKey = item.slice(0, item.indexOf("_"));
+
+      if (Object.hasOwn(filter, objectKey)) {
+        filter[objectKey]["$in"].push(new RegExp(req.query[item], "i"));
+      } else {
+        filter[objectKey] = { $in: [new RegExp(req.query[item], "i")] };
+      }
+    } else if (item.includes("_ne")) {
+      filter[item.slice(0, item.indexOf("_ne"))] = { $nin: query[item] };
+    } else if (item.includes("_gte")) {
+      const objectKey = item.slice(0, item.indexOf("_gte"));
+
+      if (Object.hasOwn(filter, objectKey)) {
+        filter[objectKey]["$gte"] = query[item];
+      } else {
+        filter[objectKey] = { $gte: query[item] };
+      }
+    } else if (item.includes("_lte")) {
+      const objectKey = item.slice(0, item.indexOf("_lte"));
+
+      if (Object.hasOwn(filter, objectKey)) {
+        filter[objectKey]["$lte"] = query[item];
+      } else {
+        filter[objectKey] = { $lte: query[item] };
+      }
+    } else if (item === "q" && query["q"]) {
+      filter["$text"] = { $search: `"${query["q"]}"` };
+    } else {
+      if (Object.hasOwn(filter, item)) {
+        filter[item]["$in"].push(query[item]);
+      } else {
+        filter[item] = { $in: [query[item]] };
+      }
     }
+  });
+
+  try {
+    const orders = await Order.find(filter)
+      .select("-__v")
+      .populate(populate)
+      .skip(start)
+      .limit(limit)
+      .sort(sortOpt)
+      .exec();
+    res.json(orders);
+  } catch (error) {
+    res.status(400).json({
+      message: "Không tìm thấy đơn hàng",
+      error,
+    });
+  }
 };
 
 export const update = async (req, res) => {
-    const filter = { _id: req.params.id };
-    const update = req.body;
-    const options = { new: true };
+  const filter = { _id: req.params.id };
+  const update = req.body;
+  const options = { new: true };
 
-    try {
-        const order = await Order.findOneAndUpdate(filter, update, options).exec();
-        res.json(order);
-    } catch (error) {
-        res.status(400).json({
-            message: "Cập nhật đơn hàng thất bại",
-            error
-        });
-    }
+  try {
+    const order = await Order.findOneAndUpdate(filter, update, options).exec();
+    res.json(order);
+  } catch (error) {
+    res.status(400).json({
+      message: "Cập nhật đơn hàng thất bại",
+      error,
+    });
+  }
 };
 
 export const remove = async (req, res) => {
-    const filter = { _id: req.params.id };
+  const filter = { _id: req.params.id };
 
-    try {
-        const order = await Order.findOneAndDelete(filter).exec();
-        res.json(order);
-    } catch (error) {
-        res.status(400).json({
-            message: "Xóa đơn hàng không thành công",
-            error
-        });
+  try {
+    const order = await Order.findOneAndDelete(filter).exec();
+    res.json(order);
+  } catch (error) {
+    res.status(400).json({
+      message: "Xóa đơn hàng không thành công",
+      error,
+    });
+  }
+};
+
+function sortObject(obj) {
+  var sorted = {};
+  var str = [];
+  var key;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      str.push(encodeURIComponent(key));
     }
+  }
+  str.sort();
+  for (key = 0; key < str.length; key++) {
+    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+  }
+  return sorted;
+}
+export const convertDate = (date) => {
+  const dateConvert = new Date(date);
+  return dateConvert.getTime();
+};
+export const createPaymentUrl = async (req, res) => {
+  try {
+    var ipAddr =
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+
+    var tmnCode = "CPJR5FXB";
+    var secretKey = "ZRGGNONEPRDMXCHFQAADTSTLVFFFFJJN";
+    var vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+    var returnUrl = "http://localhost:3000/pay/success";
+
+    // console.log(tmnCode, secretKey, vnpUrl, returnUrl);
+
+    var date = new Date();
+
+    var createDate = format.asString("yyyyMMddhhmmss", date);
+    var orderId = format.asString("HHmmss", date);
+    console.log("date", createDate, orderId);
+    // var createDate = convertDate(date);
+    // var orderId = convertDate(date);
+
+    var orderType = "fashion";
+
+    var amount = req.body.amount;
+    var bankCode = req.body.bankCode;
+    var orderInfo = req.body.orderDescription;
+
+    var locale = "vn";
+    var currCode = "VND";
+    var vnp_Params = {};
+    vnp_Params["vnp_Version"] = "2.1.0";
+    vnp_Params["vnp_Command"] = "pay";
+    vnp_Params["vnp_TmnCode"] = tmnCode;
+    // vnp_Params['vnp_Merchant'] = '';
+    vnp_Params["vnp_Locale"] = locale;
+    vnp_Params["vnp_CurrCode"] = currCode;
+    vnp_Params["vnp_TxnRef"] = orderId;
+    vnp_Params["vnp_OrderInfo"] = orderInfo;
+    vnp_Params["vnp_OrderType"] = orderType;
+    vnp_Params["vnp_Amount"] = amount * 100;
+    vnp_Params["vnp_ReturnUrl"] = returnUrl;
+    vnp_Params["vnp_IpAddr"] = ipAddr;
+    vnp_Params["vnp_CreateDate"] = createDate;
+    if (bankCode !== null && bankCode !== "") {
+      vnp_Params["vnp_BankCode"] = bankCode;
+    }
+    console.log(vnp_Params["vnp_BankCode"]);
+    vnp_Params = sortObject(vnp_Params);
+    var querystring = require("qs");
+    var signData = querystring.stringify(vnp_Params, { encode: false });
+    var crypto = require("crypto");
+    var hmac = crypto.createHmac("sha512", secretKey);
+    var signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
+    vnp_Params["vnp_SecureHash"] = signed;
+    vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
+
+    res.json(vnpUrl);
+  } catch (error) {
+    res.status(400).json({
+      message: "Không tạo được đường dẫn thanh toán",
+    });
+  }
+};
+export const vnpayReturn = async (req, res) => {
+  var vnp_Params = req.query;
+
+  var secureHash = vnp_Params["vnp_SecureHash"];
+
+  delete vnp_Params["vnp_SecureHash"];
+  delete vnp_Params["vnp_SecureHashType"];
+
+  vnp_Params = sortObject(vnp_Params);
+
+  var tmnCode = process.env.VNP_TMNCODE;
+  var secretKey = process.env.VNP_HASHSECRET;
+
+  var querystring = require("qs");
+  var signData = querystring.stringify(vnp_Params, { encode: false });
+  var crypto = require("crypto");
+  var hmac = crypto.createHmac("sha512", secretKey);
+  var signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
+
+  if (secureHash === signed) {
+    //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
+
+    res.render("success", { code: vnp_Params["vnp_ResponseCode"] });
+  } else {
+    res.render("success", { code: "97" });
+  }
+};
+export const vnpayIpn = async (req, res) => {
+  var vnp_Params = req.query;
+  var secureHash = vnp_Params["vnp_SecureHash"];
+
+  delete vnp_Params["vnp_SecureHash"];
+  delete vnp_Params["vnp_SecureHashType"];
+
+  vnp_Params = sortObject(vnp_Params);
+
+  var secretKey = process.env.VNP_HASHSECRET;
+  var querystring = require("qs");
+  var signData = querystring.stringify(vnp_Params, { encode: false });
+  var crypto = require("crypto");
+  var hmac = crypto.createHmac("sha512", secretKey);
+  var signed = hmac.update(new Buffer(signData, "utf-8")).digest("hex");
+
+  if (secureHash === signed) {
+    var orderId = vnp_Params["vnp_TxnRef"];
+    var rspCode = vnp_Params["vnp_ResponseCode"];
+    //Kiem tra du lieu co hop le khong, cap nhat trang thai don hang va gui ket qua cho VNPAY theo dinh dang duoi
+    res.status(200).json({ RspCode: "00", Message: "success" });
+  } else {
+    res.status(200).json({ RspCode: "97", Message: "Fail checksum" });
+  }
 };
--- routes/order.js
+++ routes/order.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { create, list, read, remove, update } from "../controllers/order";
+import {
+  create,
+  createPaymentUrl,
+  list,
+  read,
+  remove,
+  update,
+  vnpayIpn,
+  vnpayReturn,
+} from "../controllers/order";
 import { userById } from "../controllers/user";
-
 
 const router = Router();
 
@@ -12,5 +20,7 @@ router.put("/orders/:id/:userId", update);
 router.delete("/orders/:id", remove);
 
 router.param("userId", userById);
-
+router.post("/orders/create_payment_url", createPaymentUrl);
+router.get("/orders/vnpay_return", vnpayReturn);
+router.get("/orders/vnpay_ipn", vnpayIpn);
 export default router;
